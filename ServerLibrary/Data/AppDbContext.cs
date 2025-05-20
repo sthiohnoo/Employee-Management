70f75f6b3029a5bc8f6f64ce4d12e475abca5024
sthiohnoo <hiroyuki.ohno@stud.h-da.de>
2025-05-20 11:29:08 +0200

    feat: add entites and setup connection to db including migration

---
+++ ServerLibrary/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using BaseLibrary.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ServerLibrary.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public DbSet<GeneralDepartment> GeneralDepartments { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Branch> Branches { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Town> Towns { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    }
+}
